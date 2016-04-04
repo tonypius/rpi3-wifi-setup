4c658355527b872f8330f7ffa8570f04636e693f
tonypius <tony@tigerinnovations.org>
2016-04-04 13:42:22 +0530

    Added boot config files replacement

--- install.sh
+++ install.sh
@@ -7,12 +7,12 @@ echo "Done installing both..."
 
 echo "Configure your wlan0 interface with a static IP..."
 
-sudo mv /etc/dhcpcd.conf /etc/dhcpcd.conf.orig  
-sudo mv /dhcpd.conf /etc/dhcpcd.conf 
+#sudo mv /etc/dhcpcd.conf /etc/dhcpcd.conf.orig  
+sudo cp dhcpd.conf /etc/dhcpcd.conf 
 
 
-sudo mv /etc/network/interfaces /etc/network/interfaces.orig  
-sudo mv /interfaces /etc/network/interfaces 
+#sudo mv /etc/network/interfaces /etc/network/interfaces.orig  
+sudo cp interfaces /etc/network/interfaces 
 
 
 echo "Done configuring wlan0 interface with a static IP..."
@@ -21,22 +21,29 @@ sudo service dhcpcd restart
 
 echo "Configuring hostapd..."
 
-sudo mv /etc/hostapd/hostapd.conf /etc/hostapd/hostapd.conf.orig  
-sudo mv /hostapd.conf /etc/hostapd/hostapd.conf 
+#sudo mv /etc/hostapd/hostapd.conf /etc/hostapd/hostapd.conf.orig  
+sudo cp hostapd.conf /etc/hostapd/hostapd.conf 
 
-sudo mv /etc/default/hostapd /etc/default/hostapd.orig  
-sudo mv /hostapd /etc/default/hostapd 
+#sudo mv /etc/default/hostapd /etc/default/hostapd.orig  
+sudo cp hostapd /etc/default/hostapd 
 
 echo "Done configuring hostapd..."
 
 echo "Configuring dnsmasq..."
 
-sudo mv /etc/dnsmasq.conf /etc/dnsmasq.conf.orig  
-sudo mv /dnsmasq.conf /etc/dnsmasq.conf 
+#sudo mv /etc/dnsmasq.conf /etc/dnsmasq.conf.orig  
+sudo cp dnsmasq.conf /etc/dnsmasq.conf 
 
 echo "Done configuring dnsmasq..."
 
 sudo service hostapd start  
 sudo service dnsmasq start 
+
+echo "Changing boot configurations..."
+
+sudo cp cmdline.txt /boot/cmdline.txt
+sudo cp config.txt /boot/config.txt
+
+echo " DOne replacing boot config files"
 
 echo "ALL DONE !!! Run 'sudo reboot' to complete"
